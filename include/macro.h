c73ecd3100497662f8a544dd6f65f9b95559e770
wornwinter <darrena092@gmail.com>
2015-02-05 09:13:47 +0000

    Fix endianness in MMU

--- include/macro.h
+++ include/macro.h
@@ -1,7 +1,7 @@
 #ifndef MACRO_H
 #define MACRO_H
 
-#define BytesToWord(A, B) ((A << 8) | B)
+#define BytesToWord(A, B) ((B << 8) | A)
 #define UpperByte(A) ((A & 0xFF00) >> 8)
 #define LowerByte(A) (A & 0x00FF)
 
