3c65da4f34f5d5be69e41299ca4198c327857258
Quaker762 <Jooster669@gmail.com>
2015-02-20 19:33:01 +1100

    Texture filter
    Updated the texture filter to get rid of the yucky blur if we scale up a
    bit

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -46,8 +46,8 @@ void c_Canvas::OnPaint(wxPaintEvent& event)
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 160, 144, 0, GL_RGB, GL_UNSIGNED_BYTE, &pixels);
 
 	glBegin(GL_QUADS);
