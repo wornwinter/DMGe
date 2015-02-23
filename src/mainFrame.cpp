8027284704891ac108963d99332a40dc54b5d218
Darren Anderson <darrena092@gmail.com>
2015-02-23 12:13:48 +0000

    Setting rom back to tetris.

--- src/mainFrame.cpp
+++ src/mainFrame.cpp
@@ -44,9 +44,9 @@ mainFrame::mainFrame(const wxString& title, const wxPoint& pos, const wxSize& si
     glCanvas = new c_Canvas(panel, -1, wxDefaultPosition, wxSize(160, 144));
 
 
-    gameboy = new c_GameBoy("roms/opus5.gb", glCanvas);
+    //gameboy = new c_GameBoy("roms/opus5.gb", glCanvas);
     //gameboy = new c_GameBoy("roms/SPACE.GB", glCanvas);
-    //gameboy = new c_GameBoy("roms/tetris.gb", glCanvas);
+    gameboy = new c_GameBoy("roms/tetris.gb", glCanvas);
     //gameboy = new c_GameBoy("roms/instr_timing.gb", glCanvas);
     gameboy->pause = true;
 
